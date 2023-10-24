f45a4371710a85b7a8da4b94eebc01a8d3970073
Balazs Szel <szel.balazs@gmail.com>
2023-10-24 17:12:23 +0200

    Documentation

--- app/src/main/java/cbl_project/Collider.java
+++ app/src/main/java/cbl_project/Collider.java
@@ -1,10 +1,7 @@
 package cbl_project;
 
-import java.awt.BasicStroke;
-import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Collider {
@@ -16,19 +13,23 @@ public class Collider {
     RocketToRed rocketToRed;
     RocketToYellow rocketToYellow;
 
-
-    void checkBulletCoolision(PlayerRed playerRed){
-        int i =0;
-        while(i< bullets.size()){
+    /**
+     * 
+     * @param playerRed
+     */
+    void checkBulletCoolision(PlayerRed playerRed) {
+        int i = 0;
+        while (i < bullets.size()) {
             bullet = bullets.get(i);
-            if(bullet.ownerId == playerRed.id){
+            if (bullet.ownerId == playerRed.id) {
                 i++;
                 continue;
             }
-            // TODO: Change for tilted rectangles for Angles
-            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y, bullet.width, bullet.height);
-            Rectangle playerRectangle = new Rectangle(playerRed.x, playerRed.y, playerRed.width , playerRed.height);
-            if(bulletRectangle.intersects(playerRectangle)){
+            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y,
+                bullet.width, bullet.height);
+            Rectangle playerRectangle = new Rectangle(playerRed.x, playerRed.y,
+                playerRed.width, playerRed.height);
+            if (bulletRectangle.intersects(playerRectangle)) {
                 bullets.remove(i);
                 playerRed.hp -= 10;
                 continue;
@@ -37,17 +38,23 @@ public class Collider {
         }
     }
 
-    void checkBulletCoolision(PlayerYellow playerYellow){
-        int i =0;
-        while(i< bullets.size()){
+    /**
+     * 
+     * @param playerYellow
+     */
+    void checkBulletCoolision(PlayerYellow playerYellow) {
+        int i = 0;
+        while (i < bullets.size()) {
             bullet = bullets.get(i);
-            if(bullet.ownerId == playerYellow.id){
+            if (bullet.ownerId == playerYellow.id) {
                 i++;
                 continue;
             }
-            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y, bullet.width, bullet.height);
-            Rectangle playerRectangle = new Rectangle(playerYellow.x,playerYellow.y,playerYellow.width , playerYellow.height);
-            if(bulletRectangle.intersects(playerRectangle)){
+            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y,
+                bullet.width, bullet.height);
+            Rectangle playerRectangle = new Rectangle(playerYellow.x, playerYellow.y,
+                playerYellow.width, playerYellow.height);
+            if (bulletRectangle.intersects(playerRectangle)) {
                 bullets.remove(i);
                 playerYellow.hp -= 10;
                 continue;
@@ -56,17 +63,20 @@ public class Collider {
         }
     }
 
-    void checkRocketCoolisionRed(){
-        int i =0;
+    /**
+     * 
+     */
+    void checkRocketCoolisionRed() {
+        int i = 0;
         int dist = 0;
-        while(i< rocketsToRed.size()){
+        while (i < rocketsToRed.size()) {
             rocketToRed = rocketsToRed.get(i);
             double x1 = rocketToRed.x;
             double y1 = rocketToRed.y;
-            double x2 = gp.playerRed.x + gp.playerRed.width/2;
-            double y2 = gp.playerRed.y + gp.playerRed.height/2;
-            dist = (int)Point2D.distance(x1,y1,x2,y2);
-            if(dist<rocketToRed.rocketDetonationDist){
+            double x2 = gp.playerRed.x + gp.playerRed.width / 2;
+            double y2 = gp.playerRed.y + gp.playerRed.height / 2;
+            dist = (int) Point2D.distance(x1, y1, x2, y2);
+            if (dist < rocketToRed.rocketDetonationDist) {
                 rocketsToRed.remove(i);
                 gp.playSoundEffect(5);
                 gp.playerRed.hp -= 10;
@@ -76,17 +86,20 @@ public class Collider {
         }
     }
 
-    void checkRocketCoolisionYellow(){
-        int i =0;
+    /**
+     * 
+     */
+    void checkRocketCoolisionYellow() {
+        int i = 0;
         int dist = 0;
-        while(i< rocketsToYellow.size()){
+        while (i < rocketsToYellow.size()) {
             rocketToYellow = rocketsToYellow.get(i);
             double x1 = rocketToYellow.x;
             double y1 = rocketToYellow.y;
-            double x2 = gp.playerYellow.x + gp.playerYellow.width/2;
-            double y2 = gp.playerYellow.y + gp.playerYellow.height/2;
-            dist = (int)Point2D.distance(x1,y1,x2,y2);
-            if(dist<rocketToYellow.rocketDetonationDist){
+            double x2 = gp.playerYellow.x + gp.playerYellow.width / 2;
+            double y2 = gp.playerYellow.y + gp.playerYellow.height / 2;
+            dist = (int) Point2D.distance(x1, y1, x2, y2);
+            if (dist < rocketToYellow.rocketDetonationDist) {
                 rocketsToYellow.remove(i);
                 gp.playSoundEffect(5);
                 gp.playerYellow.hp -= 10;
@@ -95,15 +108,21 @@ public class Collider {
             i++;
         }
     }
+
+    /**
+     * 
+     * @param opponent
+     */
     void checkOpponentBulletCollisioN(Opponent opponent){
-        int i =0;
-        while(i< bullets.size()){
+        int i = 0;
+        while (i < bullets.size()) {
             bullet = bullets.get(i);
-            if(bullet.ownerId == opponent.id){
+            if (bullet.ownerId == opponent.id) {
                 i++;
                 continue;
             }
-            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y, bullet.width, bullet.height);
+            Rectangle bulletRectangle = new Rectangle(bullet.x, bullet.y,
+                bullet.width, bullet.height);
             Rectangle playerRectangle = new Rectangle(opponent.x,opponent.y,opponent.width , opponent.height);
             if(bulletRectangle.intersects(playerRectangle)){
                 bullets.remove(i);
