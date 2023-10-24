f8a64bd4ba7ffcdae23cb75c34bc059ec761282a
Balazs Szel <szel.balazs@gmail.com>
2023-10-24 16:35:24 +0200

    Added some documentation

--- app/src/main/java/cbl_project/Bullet.java
+++ app/src/main/java/cbl_project/Bullet.java
@@ -1,30 +1,42 @@
 package cbl_project;
 
-import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-
+/**
+ * The class handles bullets shot by spaceships.
+ */
 public class Bullet {
-
     GamePanel gp;
     KeyHandler keyH;
     BufferedImage[] bulletsSpirits = new BufferedImage[4];
-    double yOffset, xOffset;
-    int x,y;
+    double yOffset;
+    double xOffset;
+    int x;
+    int y;
     int speed = 5;
     int angle;
-    int spiritCounter=0;
-    int spiritNumber =0;
+    int spiritCounter = 0;
+    int spiritNumber = 0;
     int ownerId;
     int width = 10;
     int height = 10;
-    public Bullet(GamePanel gp, KeyHandler keyH, int xStart, int yStart, int angleStart, int ownerId) {
+    
+    /**
+     * Initializes the bullet class.
+     * @param gp The gamepanel class.
+     * @param keyH Keyhandler, handles key inputs.
+     * @param xStart The starting x position of the bullet.
+     * @param yStart The starting y position of the bullet.
+     * @param angleStart The starting angle of the bullet.
+     * @param ownerId The ID of the spaceship that shot the bullet.
+     */
+    public Bullet(GamePanel gp, KeyHandler keyH,
+        int xStart, int yStart, int angleStart, int ownerId) {
         this.gp = gp;
         this.keyH = keyH;
         x = xStart;
@@ -34,17 +46,26 @@ public class Bullet {
         setUp();
     }
 
-    public BufferedImage rotateImage(BufferedImage img, double degrees){
+    /**
+     * Rotates an image.
+     * @param img The image
+     * @param degrees The degree by which the image has to be rotated.
+     * @return The rotated image.
+     */
+    public BufferedImage rotateImage(BufferedImage img, double degrees) {
         double theta = Math.toRadians(degrees);
 
         AffineTransform tx = new AffineTransform();
         tx.rotate(theta, img.getWidth() / 2, img.getHeight() / 2);
 
-        AffineTransformOp op = new AffineTransformOp(tx,AffineTransformOp.TYPE_BILINEAR);
+        AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
         return op.filter(img, null);
     }
 
-    void setUp(){
+    /**
+     * Reads the image files, prints error in case of an exception.
+     */
+    void setUp() {
         try {
             bulletsSpirits[0] = ImageIO.read(getClass().getResourceAsStream("/bullet1.png"));
             bulletsSpirits[0] = rotateImage(bulletsSpirits[0], angle);
@@ -57,24 +78,32 @@ public class Bullet {
 
             System.out.println("[INFO] Bullet templates downloaded succesfuly");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
             System.out.println("[ERROR] Image load failure!");
         }
     }
-    void update(){
-        xOffset = Math.sin(Math.toRadians(angle))*speed;
-        yOffset = Math.cos(Math.toRadians(angle))*speed;
+
+    /**
+     * Updates the position and the animation of the bullet.
+     */
+    void update() {
+        xOffset = Math.sin(Math.toRadians(angle)) * speed;
+        yOffset = Math.cos(Math.toRadians(angle)) * speed;
         x += xOffset;
         y -= yOffset;
 
         spiritCounter++;
-        if(spiritCounter>10){
-            spiritNumber = (spiritNumber+1 )%4;
+        if (spiritCounter > 10) {
+            spiritNumber = (spiritNumber + 1) % 4;
             spiritCounter = 0;
         }
     }
-    void draw(Graphics2D g2){
+
+    /**
+     * Draws the bullet.
+     * @param g2 The graphics2D object that draws the image.
+     */
+    void draw(Graphics2D g2) {
         g2.drawImage(bulletsSpirits[spiritNumber], null, x, y);
     } 
 
